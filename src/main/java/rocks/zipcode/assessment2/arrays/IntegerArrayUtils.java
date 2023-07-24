1cbe0bee6e3194f7d6ba38f02188fa3028e8ad08
DestanyM <destanymcclellan22@gmail.com>
2023-07-24 10:41:18 -0400

    Array section complete, passing all tests

--- src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/IntegerArrayUtils.java
@@ -1,26 +1,37 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author leon on 28/11/2018.
  */
 public class IntegerArrayUtils {
     /**
-     * @param integerArray - array to have value added to it
+     * @param integerArray   - array to have value added to it
      * @param valueToBeAdded - value to be added to the end of the array
      * @return - identical array with one additional element of `valueToBeAdded` at the end of the array
      */
     public static Integer[] add(Integer[] integerArray, Integer valueToBeAdded) {
-        return null;
+        int arrayLength = integerArray.length;
+        Integer[] newArray = Arrays.copyOf(integerArray, arrayLength + 1);
+        newArray[arrayLength] = valueToBeAdded;
+        return newArray;
+        // return null;
     }
 
     /**
-     * @param integerArray - array to be manipulated
-     * @param indexToInsertAt - index of the element to be inserted at
+     * @param integerArray      - array to be manipulated
+     * @param indexToInsertAt   - index of the element to be inserted at
      * @param valueToBeInserted - value of the element to be inserted
      * @return `integerArray` with `valueToBeInserted` at index number `indexToInsertAt`
      */
     public static Integer[] replace(Integer[] integerArray, int indexToInsertAt, Integer valueToBeInserted) {
-        return null;
+        int arrayLength = integerArray.length;
+        Integer[] newArray = Arrays.copyOf(integerArray, arrayLength);
+        newArray[indexToInsertAt] = valueToBeInserted;
+        return newArray;
+        //return null;
     }
 
     /**
@@ -29,7 +40,11 @@ public class IntegerArrayUtils {
      * @return element located at `indexToFetch`
      */
     public static Integer get(Integer[] integerArray, Integer indexToFetch) {
-        return null;
+        int arrayLength = integerArray.length;
+        Integer[] newArray = Arrays.copyOf(integerArray, arrayLength + 1);
+        newArray[indexToFetch] = indexToFetch;
+        return integerArray[indexToFetch];
+        //return null;
     }
 
     /**
@@ -37,15 +52,35 @@ public class IntegerArrayUtils {
      * @return identical array with even-values incremented by 1 and odd-values decremented by 1
      */
     public static Integer[] incrementEvenDecrementOdd(Integer[] integerArray) {
-        return null;
+        Integer[] changedArray = new Integer[integerArray.length];
+        for (int i = 0; i < integerArray.length; i++) {
+            if (integerArray[i] % 2 == 0) {
+                changedArray[i] = integerArray[i] + 1;
+            } else {
+                changedArray[i] = integerArray[i] - 1;
+            }
+        }
+        return changedArray;
+        //return null;
     }
+
 
     /**
      * @param integerArray - array to be evaluated
      * @return identical array with even-values incremented by 1
      */
     public static Integer[] incrementEven(Integer[] integerArray) {
-        return null;
+        Integer[] incrementEven = new Integer[integerArray.length];
+        for (int i = 0; i < integerArray.length; i++) {
+            if (integerArray[i] % 2 == 0) {
+                incrementEven[i] = integerArray[i] + 1;
+            } else {
+                incrementEven[i] = integerArray[i];
+            }
+        }
+
+        return incrementEven;
+        //return null;
     }
 
     /**
@@ -53,6 +88,17 @@ public class IntegerArrayUtils {
      * @return identical array with odd-values decremented by 1
      */
     public static Integer[] decrementOdd(Integer[] input) {
-        return null;
+        Integer[] decrementOdd = new Integer[input.length];
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] % 2 != 0) {
+                decrementOdd[i] = input[i] - 1;
+            } else {
+                decrementOdd[i] = input[i];
+            }
+        }
+
+            return decrementOdd;
+            //return null;
+
     }
 }
